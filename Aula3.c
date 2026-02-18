b06056489fd9ebf053df813712bee21ddcc5a488
JQuirino12 <joao.quirino21@hotmail.com>
2026-02-18 20:00:18 -0300

    Update Aula3.c

--- Aula3.c
+++ Aula3.c
@@ -1,1 +1,66 @@
 
+#include <stdio.h>
+
+#define TAMANHO 5
+#define VAZIO 0
+
+void carregar(int vetor[]) {
+  
+}
+
+void exibir(int vetor[]) {
+  
+}
+
+void removerRa(int vetor[], int ra) {
+  
+}
+
+int contarRa(int vetor[], int ra) {
+  return -1;
+}
+
+void maiorRa(int vetor[]) {
+  return -1;
+}
+
+void menu() {
+  int opc = 0;
+  do {
+    printf("\n=====SYSTEM=====\n");
+    printf("1. Carregar \n");
+    printf("2. Exibir \n");
+    printf("3. Remover \n");
+    printf("4.contar Ra \n");
+    printf("5. maior Ra");
+    scanf("%i", &opc);
+    
+    switch (opc) {
+      case 1: {
+        break;
+      }
+      case 2: {
+        break;
+      }
+      case 3: {
+        break;
+      }
+      case 4: {
+        break;
+      }
+      case 5: {
+        break;
+      }
+      
+    } while (opc = 9);
+    
+  }
+ 
+
+  
+}
+
+int main(int )
+{
+    
+}
