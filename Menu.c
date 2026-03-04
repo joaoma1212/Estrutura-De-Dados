70bd57a16f70b9221b80c24108d1ef1e3c713dfb
JQuirino12 <joao.quirino21@hotmail.com>
2026-03-04 20:26:56 -0300

    Create Menu.c

---
+++ Menu.c
@@ -0,0 +1,24 @@
+void menu() {
+  int opc = 0;
+  int valra = 0;
+  int i = 0, p = 0;
+  
+  do {
+  
+  printf("\n====== FILA DE ALUNO ======\n");
+    printf("1 - Adicionar\n");
+    printf("2 - Removerr\n");
+    printf("3 - Exibir\n");
+    printf("9 - Sair\n");
+    printf("digite opcao: ");
+    scanf("%i", &opc)
+    
+    for (opc == 1){
+      
+    } else if (opc == 2) {
+      
+    } else if (opc == 3) {
+      
+    }
+    
+} while (opc != 9);
